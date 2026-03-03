74405bde6c94aed1d3624a9aeb89340c5ea8959e
simon <sdmorris1@widener.edu>
2026-03-03 12:12:02 -0500

    portrait mode

--- app/src/main/java/com/example/tempapp/MainActivity.kt
+++ app/src/main/java/com/example/tempapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tempapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,93 +37,204 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-            var text1 by remember { mutableStateOf("0")}
-            var text2 by remember { mutableStateOf("0")}
-            var text3 by remember { mutableStateOf("0")}
+            CalcScreen()
+        }
+    }
+}
 
-            val num1 = text1.toFloat()
-            val num2 = text2.toFloat()
-            val num3 = text3.toFloat()
+@Composable
+fun CalcScreen(modifier: Modifier = Modifier){
+    if(LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT){
+        CalcPortrait()
+    } else {
+        CalcLandscape()
+    }
+}
 
-            val subTotal = num1 + num2 + num3
-            val tax = subTotal * 0.06
-            val total = tax + subTotal
+@Composable
+fun CalcLandscape(modifier: Modifier = Modifier){
+    var text1 by remember { mutableStateOf("0")}
+    var text2 by remember { mutableStateOf("0")}
+    var text3 by remember { mutableStateOf("0")}
 
-            TempAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        modifier = Modifier.padding(32.dp)
-                    ) {
-                    Item(
+    val num1 = text1.toFloat()
+    val num2 = text2.toFloat()
+    val num3 = text3.toFloat()
+
+    val subTotal = num1 + num2 + num3
+    val tax = subTotal * 0.06
+    val total = tax + subTotal
+
+    TempAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = modifier.fillMaxWidth().padding(32.dp),
+                verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+//                    modifier = Modifier.padding(32.dp)
+                ) {
+                    ItemLand(
                         modifier = Modifier.padding(innerPadding),
                         1,
                         onValueChange = { text1 = it },
                         text = text1
                     )
-                    Item(
+                    ItemLand(
                         modifier = Modifier.padding(innerPadding),
                         2,
                         onValueChange = { text2 = it },
                         text = text2
                     )
-                    Item(
+                    ItemLand(
                         modifier = Modifier.padding(innerPadding),
                         3,
                         onValueChange = { text3 = it },
                         text = text3
                     )
+                }
 
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            modifier = Modifier.padding(top=300.dp)
 
-                        ){
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+//                    modifier = Modifier.padding(32.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+//                        modifier = Modifier.padding(top = 300.dp)
 
+                    ) {
                         Text(
-                            text="Subtotal:",
+                            text = "Subtotal:",
                             fontSize = 32.sp
                         )
-                            Text(
-                                text="$$subTotal",
-                                fontSize = 32.sp
-                            )
-                        }
-
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceEvenly
-                        ) {
-
-
-                            Text(
-                                text = "Tax (6%):",
-                                fontSize = 32.sp
-                            )
-                            Text(
-                                text = "$$tax",
-                                fontSize = 32.sp
-                            )
-                        }
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            modifier = Modifier.padding(top=20.dp)
-
-                        ) {
-
-                            Text(
-                                text = "Total:",
-                                fontSize = 32.sp,
-                            )
-                            Text(
-                                text = "$$total",
-                                fontSize = 32.sp
-                            )
-                        }
+                        Text(
+                            text = "$$subTotal",
+                            fontSize = 32.sp
+                        )
                     }
 
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
 
 
+                        Text(
+                            text = "Tax (6%):",
+                            fontSize = 32.sp
+                        )
+                        Text(
+                            text = "$$tax",
+                            fontSize = 32.sp
+                        )
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        modifier = Modifier.padding(top = 20.dp)
 
+                    ) {
+
+                        Text(
+                            text = "Total:",
+                            fontSize = 32.sp,
+                        )
+                        Text(
+                            text = "$$total",
+                            fontSize = 32.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CalcPortrait(modifier: Modifier = Modifier){
+    var text1 by remember { mutableStateOf("0")}
+    var text2 by remember { mutableStateOf("0")}
+    var text3 by remember { mutableStateOf("0")}
+
+    val num1 = text1.toFloat()
+    val num2 = text2.toFloat()
+    val num3 = text3.toFloat()
+
+    val subTotal = num1 + num2 + num3
+    val tax = subTotal * 0.06
+    val total = tax + subTotal
+
+    TempAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.padding(32.dp)
+            ) {
+                Item(
+                    modifier = Modifier.padding(innerPadding),
+                    1,
+                    onValueChange = { text1 = it },
+                    text = text1
+                )
+                Item(
+                    modifier = Modifier.padding(innerPadding),
+                    2,
+                    onValueChange = { text2 = it },
+                    text = text2
+                )
+                Item(
+                    modifier = Modifier.padding(innerPadding),
+                    3,
+                    onValueChange = { text3 = it },
+                    text = text3
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier.padding(top=300.dp)
+
+                ){
+
+                    Text(
+                        text="Subtotal:",
+                        fontSize = 32.sp
+                    )
+                    Text(
+                        text="$$subTotal",
+                        fontSize = 32.sp
+                    )
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+
+
+                    Text(
+                        text = "Tax (6%):",
+                        fontSize = 32.sp
+                    )
+                    Text(
+                        text = "$$tax",
+                        fontSize = 32.sp
+                    )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier.padding(top=20.dp)
+
+                ) {
+
+                    Text(
+                        text = "Total:",
+                        fontSize = 32.sp,
+                    )
+                    Text(
+                        text = "$$total",
+                        fontSize = 32.sp
+                    )
                 }
             }
         }
@@ -133,6 +246,25 @@ fun Item(modifier: Modifier = Modifier, itemNumber: Number, text: String, onValu
     Row(
         horizontalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = "Item #$itemNumber:",
+            modifier = modifier,
+            fontSize = 32.sp
+        )
+        TextField(
+            value = text,
+            onValueChange = onValueChange,
+        )
+    }
+}
+
+@Composable
+fun ItemLand(modifier: Modifier = Modifier, itemNumber: Number, text: String, onValueChange: (String) -> Unit) {
+    Row(
+//        horizontalArrangement = Arrangement.SpaceEvenly,
+//        modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(
